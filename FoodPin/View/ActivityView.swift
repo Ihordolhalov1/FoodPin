cb77efa1f3aa455ec94b29cca46d44845e8709fb
Ihordolhalov1 <servicegambro@gmail.com>
2023-09-20 11:42:39 +0100

    Navigation View was added

---
+++ FoodPin/View/ActivityView.swift
@@ -0,0 +1,8 @@
+//
+//  ActivityView.swift
+//  FoodPin
+//
+//  Created by Ihor Dolhalov on 19.09.2023.
+//
+
+import Foundation
---
+++ FoodPin/View/RestaurantDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantDetailView.swift
+//  FoodPin
+//
+//  Created by Ihor Dolhalov on 20.09.2023.
+//
+
+import SwiftUI
+
+struct RestaurantDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RestaurantDetailView()
+}
