ac9f11a1f04cb8e354db68c05f5b87924b20f948
Ihordolhalov1 <servicegambro@gmail.com>
2023-09-29 22:49:41 +0100

    MapKit framework was embagged

---
+++ FoodPin/Extensions/UiColor.swift
@@ -0,0 +1,8 @@
+//
+//  UiColor.swift
+//  FoodPin
+//
+//  Created by Ihor Dolhalov on 21.09.2023.
+//
+
+import Foundation
---
+++ FoodPin/View/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  FoodPin
+//
+//  Created by Ihor Dolhalov on 25.09.2023.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
